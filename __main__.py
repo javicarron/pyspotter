6130ad5c05e2bc2abf2667ab3a20fad6cfe382df
Javier Carron Duque <javicarron@gmail.com>
2017-01-18 12:26:49 +0100

    Opening local images

--- __main__.py
+++ __main__.py
@@ -20,6 +20,10 @@ from matplotlib.figure import Figure
 class CanvasPanel(wx.Panel):
     def __init__(self, parent, image=fits.open("horse.fits")):
         wx.Panel.__init__(self, parent)
+        self.SetImage(fits.open("horse.fits"))
+        
+        
+    def SetImage(self, image):
         header=image['PRIMARY'].header
         data=image['PRIMARY'].data
         image.close()
@@ -29,24 +33,24 @@ class CanvasPanel(wx.Panel):
         ax=self.fig.add_axes([0.1,0.1,0.8,0.8], projection=wcs)
         ax.set_xlabel('RA')
         ax.set_ylabel('Dec')
-        ax.imshow(data, cmap='gist_heat', origin='lower')
+        ax.imshow(np.sqrt(1+data), cmap='gist_heat', origin='lower')
         ra=ax.coords[0]
-#        ra.set_major_formatter('hh:mm:ss')
+        ra.set_major_formatter('hh:mm:ss')
         dec=ax.coords[1]
-#        dec.set_major_formatter('dd:mm:ss');
-        
+        dec.set_major_formatter('dd:mm:ss');
         self.canvas =FigureCanvas(self,-1,self.fig)
-
         
 
+        
 class MainWindow(wx.Frame):
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, title=title)
         self.CreateStatusBar()
         
+        
         #Menus
         filemenu=wx.Menu()
-        menuOpen= filemenu.Append(wx.ID_OPEN, "&Open", "Open text file")
+        menuOpen= filemenu.Append(wx.ID_OPEN, "&Open", "Open a FITS file in the computer")
         filemenu.AppendSeparator()
         menuExit= filemenu.Append(wx.ID_EXIT,"E&xit", "Terminate this program")
         
@@ -69,10 +73,6 @@ class MainWindow(wx.Frame):
         But=self.buttons =[]
         But.append(wx.Button(self, 0, "Open"))
         self.sizerbut.Add(But[0], 1, wx.EXPAND)
-        #for i in range(1,6:
-         #   self.buttons.append(wx.Button(self, 0, "Button &"+str(i)))
-         #   self.sizerbut.Add(self.buttons[i], 1, wx.EXPAND)
-            
         But.append(wx.Button(self, 1, "Exit"))
         self.sizerbut.Add(But[1], 1, wx.EXPAND)
         self.Bind(wx.EVT_BUTTON, self.OnOpen, But[0])
@@ -81,46 +81,15 @@ class MainWindow(wx.Frame):
         
         
         #Figure
-        #image=fits.open("red.fits")
-#        image=fits.open("horse.fits")
-#        header=image['PRIMARY'].header
-#        data=image['PRIMARY'].data
-#        image.close()
-#        wcs=WCS(header)
-#        
-#        self.fig=plt.figure()
-#        ax=self.fig.add_axes([0.1,0.1,0.8,0.8], projection=wcs)
-#        ax.set_xlabel('RA')
-#        ax.set_ylabel('Dec')
-#        ax.imshow(data, cmap='gist_heat', origin='lower')
-#        ra=ax.coords[0]
-#        ra.set_major_formatter('hh:mm:ss')
-#        dec=ax.coords[1]
-#        dec.set_major_formatter('dd:mm:ss');
+        global canvas
+        canvas=CanvasPanel(self)
         
-#        global prueba
-        prueba=CanvasPanel(self)
-        
-        
-#        np.sqrt(1+np.sqrt(fits.getdata('red.fits')+2))
-#        self.figure=plt.imshow(image, cmap='gist_heat', origin='lower')
-#        plt.colorbar();
-        
-        #
-#        self.figure =Figure()
-#        self.axes = self.figure.add_subplot(111)
-#        t=np.arange(0.0, 3.0, 0.01)
-#        s=np.sin(2*np.pi*t)
-#        
-#        self.axes.plot(t,s)
-#        self.canvas = FigureCanvas(self, 0, self.fig)
-#        
         
         
         #Layout
         self.mainSizer=wx.BoxSizer(wx.VERTICAL)
         self.mainSizer.Add(self.sizerbut, 0, wx.EXPAND)
-        self.mainSizer.Add(prueba, 1, wx.EXPAND)
+        self.mainSizer.Add(canvas, 1, wx.EXPAND)
 
         self.SetSizer(self.mainSizer)
         self.SetAutoLayout(1)
@@ -130,7 +99,6 @@ class MainWindow(wx.Frame):
         self.Show(True)
         
     
-        
         
         
     def OnAbout(self,e):
@@ -145,17 +113,12 @@ class MainWindow(wx.Frame):
         self.dirname=''
         dlg = wx.FileDialog(self, "Choose a file", self.dirname, "", "*.fits", wx.OPEN)
         if dlg.ShowModal() == wx.ID_OK:
-            global prueba
-            prueba.Hide()
             self.filename = dlg.GetFilename()
             self.dirname = dlg.GetDirectory()
-            image = fits.open(os.path.join(self.dirname, self.filename))
-            prueba=CanvasPanel(self,image)
-            prueba.Refresh
-            prueba.Show()
-            global mainSizer
-            mainSizer.Layout()
-        dlg.Destroy
+            img = fits.open(os.path.join(self.dirname, self.filename))
+            canvas.SetImage(img)
+            canvas.Show()
+        dlg.Destroy()
         
         
 app = wx.App(False)

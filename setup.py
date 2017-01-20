f72d3ee1d5499d378a5517b1e31c68ea7a68c657
Javier Carron Duque <javicarron@gmail.com>
2017-01-20 03:08:13 +0100

    V1 folder changes

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pyspotter',
+      version='1.0',
+      description='Astronomical image viewer',
+      author='Javier Carrón Duque',
+      author_email='javicarron@gmail.com',
+      url='https://github.com/javicarron/pyspotter.git',
+      scripts=['pyspotter']
+     )

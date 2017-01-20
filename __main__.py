cca2d3ec4bfbba65df87a4f0c40106b276623ee9
Javier Carron Duque <javicarron@gmail.com>
2017-01-20 02:48:06 +0100

    Version 1 finished

--- __main__.py
+++ __main__.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Comment
+Small program with graphical interface to visualize astronomical images in FITS
+format. It is able to change the way the intensity is visualized and the color 
+of image. It loads local images and search for images in SkyView archive with
+its built-it search function, that allows the specification of the survey used.
 """
 
 import wx
 import os
 from astropy.io import fits
-#from astropy.utils.data import download_file
 import matplotlib.pyplot as plt
 from astropy.wcs import WCS
-#from astropy.coordinates import SkyCoord
-#from astropy.nddata import Cutout2D
+from astroquery.skyview import SkyView
 import numpy as np
 from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
-from matplotlib.figure import Figure
+
+import surveys
 
 
 class CanvasPanel(wx.Panel):
+    """Panel in which all the images are plotted."""
     def __init__(self, parent, image=fits.open("horse.fits"),option="Normal"):
         wx.Panel.__init__(self, parent)
         self.SetImage(fits.open("horse.fits"))
         
-        
     def SetImage(self, image):
+        """Set an image to the canvas, setting the axes automatically"""
         header=image['PRIMARY'].header
         self.data=image['PRIMARY'].data
         image.close()
@@ -41,8 +44,8 @@ class CanvasPanel(wx.Panel):
         dec.set_major_formatter('dd:mm:ss');
         self.canvas =FigureCanvas(self,-1,self.fig)
         
-        
     def SetOption(self, option, color='gist_heat'):
+        """Change the method to display the intensity and the colormap"""
         if option=="Root":
             self.ax.imshow(np.sqrt(1+self.data), cmap=color, origin='lower')
         elif option=="Logarithmic":
@@ -51,14 +54,20 @@ class CanvasPanel(wx.Panel):
             self.ax.imshow(self.data, cmap=color, origin='lower')
         self.canvas.draw()
         
-
         
         
 class MainWindow(wx.Frame):
+    """Main window of the program"""
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, title=title)
         self.CreateStatusBar()
         
+        #Initialize variables
+        self.revinten = False
+        self.color = "gist_heat"
+        self.inten = "Normal"
+        self.band = "Optical:SDSS"
+        self.survey = "SDSSg"
         
         #Menus
         filemenu=wx.Menu()
@@ -79,23 +88,16 @@ class MainWindow(wx.Frame):
         self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
         
         
-        
-        #Buttons        
-        self.sizerbut=wx.BoxSizer(wx.HORIZONTAL)
-        But=self.buttons =[]
-        But.append(wx.Button(self, 0, "Open"))
-        self.sizerbut.Add(But[0], 1, wx.EXPAND)
-        But.append(wx.Button(self, 1, "Exit"))
-        self.sizerbut.Add(But[1], 1, wx.EXPAND)
-        self.Bind(wx.EVT_BUTTON, self.OnOpen, But[0])
-        self.Bind(wx.EVT_BUTTON, self.OnExit, But[1])
+        #Figure
+        self.panel=CanvasPanel(self)
         
         
-        #Options
-        self.revinten = False
-        self.color = "gist_heat"
-        self.inten = "Normal"
+        #Open button        
+        self.ButOpen=wx.Button(self, 0, "Open FITS file")
+        self.Bind(wx.EVT_BUTTON, self.OnOpen, self.ButOpen)        
         
+        
+        #Intensity options
         self.rbint = wx.RadioBox(self, label="Set intensity", choices=['Normal', 'Root', 'Logarithmic'])
         self.Bind(wx.EVT_RADIOBOX, self.OnInten, self.rbint)
         
@@ -103,42 +105,67 @@ class MainWindow(wx.Frame):
         self.Bind(wx.EVT_CHECKBOX, self.OnRevInten, self.revint)
         
         self.colorque = wx.StaticText(self, label="Set colormap")
-        cmaps = ['%Perceptually Uniform Sequential','viridis', 'inferno', 'plasma', 'magma','%Sequential','Blues', 'BuGn', 'BuPu','GnBu', 'Greens', 'Greys', 'Oranges', 'OrRd','PuBu', 'PuBuGn', 'PuRd', 'Purples', 'RdPu', 'Reds', 'YlGn', 'YlGnBu', 'YlOrBr', 'YlOrRd', '%Sequential (2)', 'afmhot', 'autumn', 'bone', 'cool', 'copper', 'gist_heat', 'gray', 'hot', 'pink', 'spring', 'summer', 'winter', '%Diverging', 'BrBG', 'bwr', 'coolwarm', 'PiYG', 'PRGn', 'PuOr', 'RdBu', 'RdGy', 'RdYlBu', 'RdYlGn', 'Spectral', 'seismic', '%Qualitative', 'Accent', 'Dark2', 'Paired', 'Pastel1', 'Pastel2', 'Set1', 'Set2', 'Set3', 'Vega10', 'Vega20', 'Vega20b', 'Vega20c', '%Miscellaneous', 'gist_earth', 'terrain', 'ocean', 'gist_stern', 'brg', 'CMRmap', 'cubehelix', 'gnuplot', 'gnuplot2', 'gist_ncar', 'nipy_spectral', 'jet', 'rainbow', 'gist_rainbow', 'hsv', 'flag', 'prism']
+        cmaps = ['%PERCEPTUALLY UNIFORM SEQUENTIAL','viridis', 'inferno', 'plasma', 'magma','%SEQUENTIAL','Blues', 'BuGn', 'BuPu','GnBu', 'Greens', 'Greys', 'Oranges', 'OrRd','PuBu', 'PuBuGn', 'PuRd', 'Purples', 'RdPu', 'Reds', 'YlGn', 'YlGnBu', 'YlOrBr', 'YlOrRd', '%SEQUENTIAL (2)', 'afmhot', 'autumn', 'bone', 'cool', 'copper', 'gist_heat', 'gray', 'hot', 'pink', 'spring', 'summer', 'winter', '%DIVERGING', 'BrBG', 'bwr', 'coolwarm', 'PiYG', 'PRGn', 'PuOr', 'RdBu', 'RdGy', 'RdYlBu', 'RdYlGn', 'Spectral', 'seismic', '%QUALITATIVE', 'Accent', 'Dark2', 'Paired', 'Pastel1', 'Pastel2', 'Set1', 'Set2', 'Set3', 'Vega10', 'Vega20', 'Vega20b', 'Vega20c', '%MISCELLANEOUS', 'gist_earth', 'terrain', 'ocean', 'gist_stern', 'brg', 'CMRmap', 'cubehelix', 'gnuplot', 'gnuplot2', 'gist_ncar', 'nipy_spectral', 'jet', 'rainbow', 'gist_rainbow', 'hsv', 'flag', 'prism']
         self.colorlist=wx.ComboBox(self, choices=cmaps, style=wx.CB_DROPDOWN, value='gist_heat')
         self.Bind(wx.EVT_COMBOBOX, self.OnColor, self.colorlist)
-                           
-#        self.rbcolor=wx.ColourPickerCtrl(self)
-#        self.Bind(wx.EVT_COLOURPICKER_CHANGED, self.OnColor, self.rbcolor)
+            
+
+        #Search
+        self.searchque = wx.StaticText(self, label="Object to search: ")
+        self.searchbox=wx.TextCtrl(self, style=wx.TE_PROCESS_ENTER)
+        self.Bind(wx.EVT_TEXT_ENTER, self.OnSearch, self.searchbox)
+        self.searchbut=wx.Button(self, 1, "Search")
+        self.Bind(wx.EVT_BUTTON, self.OnSearch, self.searchbut)
         
-        
-        #Figure
-        self.panel=CanvasPanel(self)
-        
+#        self.surveyque = wx.StaticText(self, label="Band and survey")
+        self.surv=surveys.list()
+        self.bandlist=wx.ComboBox(self, choices=self.surv.keys(), style=wx.CB_DROPDOWN, value='Optical:SDSS')
+        self.Bind(wx.EVT_COMBOBOX, self.OnBand, self.bandlist)
+        self.surveylist=wx.ComboBox(self, choices=self.surv[self.band], style=wx.CB_DROPDOWN, value="Choose a survey")
+        self.Bind(wx.EVT_COMBOBOX, self.OnSurvey, self.surveylist)
         
         
         #Layout
-        sizeropt=wx.BoxSizer(wx.HORIZONTAL)
-        sizeropt.Add(self.rbint, 0, wx.EXPAND)
-        sizeropt.Add(self.revint, 0, wx.EXPAND)
-        sizeropt.Add(self.colorque, 1, wx.EXPAND)
-        sizeropt.Add(self.colorlist, 1, wx.EXPAND)
+        sizeropt1=wx.BoxSizer(wx.HORIZONTAL)
+        sizeropt1.Add(self.rbint, 1, wx.EXPAND|wx.ALL, border=5)
+        sizeropt1.Add(self.revint, 1, wx.EXPAND|wx.ALL, border=5)
+        sizeropt2=wx.BoxSizer(wx.HORIZONTAL)
+        sizeropt2.Add(self.colorque, 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL|wx.ALL, border=15)
+        sizeropt2.Add(self.colorlist, 0, wx.EXPAND|wx.ALL, border=15)
+        sizeropt=wx.BoxSizer(wx.VERTICAL)
+        sizeropt.Add(sizeropt1, 0, wx.EXPAND)
+        sizeropt.Add(sizeropt2, 0, wx.EXPAND|wx.CENTER)
         
-        self.mainSizer=wx.BoxSizer(wx.VERTICAL)
-        self.mainSizer.Add(self.sizerbut, 0, wx.EXPAND)
-        self.mainSizer.Add(self.panel, 1, wx.EXPAND)
-        self.mainSizer.Add(sizeropt, 0, wx.EXPAND)
-#        self.mainSizer.Add(self.rbcolor, 0, wx.EXPAND)
+        sizersearch1=wx.BoxSizer(wx.HORIZONTAL)
+        sizersearch1.Add(self.searchque, 0, wx.EXPAND|wx.ALIGN_CENTER_VERTICAL, border=10)
+        sizersearch1.Add(self.searchbox, 1, wx.EXPAND|wx.ALL, border=10)
+        sizersearch1.Add(self.searchbut, 1, wx.EXPAND|wx.ALL, border=10)
+#        sizersearch2.Add(self.surveyque, 0, wx.EXPAND)
+        sizersearch2=wx.BoxSizer(wx.HORIZONTAL)
+        sizersearch2.Add(self.bandlist, 1, wx.EXPAND|wx.ALL, border=10)
+        sizersearch2.Add(self.surveylist, 1, wx.EXPAND|wx.ALL, border=10)
+        sizersearch=wx.BoxSizer(wx.VERTICAL)
+        sizersearch.Add(sizersearch1, 0, wx.EXPAND)
+        sizersearch.Add(sizersearch2, 0, wx.EXPAND)
+
+        sizerright=wx.BoxSizer(wx.VERTICAL)
+        sizerright.Add(self.ButOpen, 0, wx.ALL|wx.CENTER, border=50)
+        sizerright.Add(sizeropt, 1, wx.EXPAND|wx.ALL, border=25)
+        sizerright.Add(sizersearch, 1, wx.EXPAND|wx.ALL, border=25)
+        
+        self.mainSizer=wx.BoxSizer(wx.HORIZONTAL)
+        self.mainSizer.Add(self.panel, 1, wx.EXPAND | wx.ALIGN_CENTER)
+        self.mainSizer.Add(sizerright, 1, wx.EXPAND, border=5)
 
         self.SetSizer(self.mainSizer)
         self.SetAutoLayout(1)
-        #self.sizer.Fit(self)     
-        self.SetSize((650,600))
+        self.mainSizer.Fit(self)     
+        self.SetSize((1100,555))
         
         self.Show(True)
         
     def OnColor(self,event):
-#        color=self.rbcolor.GetColour()
-#        print(color)
+        """Get the colormap selected en the list"""
         if self.revinten:
             self.color=event.GetString()+"_r"
         else:
@@ -146,10 +173,12 @@ class MainWindow(wx.Frame):
         self.panel.SetOption(self.inten, self.color)
         
     def OnInten(self, event):
+        """Get the method to display the intensity"""
         self.inten= self.rbint.GetStringSelection()
         self.panel.SetOption(self.inten, self.color)
         
     def OnRevInten(self, event):
+        """Get the choice of the user to revert or not the intensity"""
         self.revinten= self.revint.GetValue()
         if self.revinten:
             self.color=self.color+"_r"
@@ -157,28 +186,49 @@ class MainWindow(wx.Frame):
             self.color=self.color[:-2]
         self.panel.SetOption(self.inten, self.color)
         
+    def OnSearch(self,event):
+        """Make an online search with SkyView"""
+        self.search=self.searchbox.GetValue()
+        image = SkyView.get_images(position=self.search, survey=self.survey)[0]
+        self.panel.SetImage(image)
+        self.panel.SetOption(self.inten,self.color)
+        
+    def OnBand(self, event):
+        """Get the prefered band in which the search has to be done"""
+        self.band=event.GetString()
+        self.survey=self.surv[self.band][0]
+        self.surveylist.Clear()
+        self.surveylist.AppendItems(self.surv[self.band])
+        self.surveylist.SetValue(self.survey)
+        
+    def OnSurvey(self, event):
+        """Get the survey to use in the search"""
+        self.survey=event.GetString()
+        
     def OnAbout(self,e):
-        dlg = wx.MessageDialog(self, "A small astronomical image analyzer", "About Pyspotter", wx.OK)
+        """Show the About Menu"""
+        dlg = wx.MessageDialog(self, "A small astronomical image analyzer by Javier Carrón Duque in 2017.\n \nViewer with graphical interface to visualize astronomical images in FITS format. It is able to change the way the intensity is visualized and the color of image. It loads local images and search for images in SkyView archive with its built-it search function, that allows the specification of the survey used. \n \nModules used: wx, os, astropy, astroquery, matplotlib, numpy. All credits to the original authors", "About Pyspotter", wx.OK)
         dlg.ShowModal()
         dlg.Destroy()
         
     def OnExit(self,e):
+        """Terminate the program"""
         self.Close(True)
         
     def OnOpen(self,e):
+        """Open a FITS file"""
         self.dirname=''
         dlg = wx.FileDialog(self, "Choose a file", self.dirname, "", "*.fits", wx.OPEN)
         if dlg.ShowModal() == wx.ID_OK:
             self.filename = dlg.GetFilename()
             self.dirname = dlg.GetDirectory()
             img = fits.open(os.path.join(self.dirname, self.filename))
-#            option= self.rbint.GetStringSelection()
             self.panel.SetImage(img)
             self.panel.SetOption(self.inten,self.color)
             self.panel.Show()
         dlg.Destroy()
         
-        
+#My python folder weights too much: it weights Pi tons.        
 app = wx.App(False)
 frame = MainWindow(None, "Pyspotter")
 app.MainLoop()
